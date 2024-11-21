a678f4f7324e00436545f7ce8f94eb2b6d536ffa
jfegasu <jfegasu@gmail.com>
2024-11-20 21:06:22 -0500

    .

--- PROY/PLOGIN/utils/Audita.py
+++ PROY/PLOGIN/utils/Audita.py
@@ -7,7 +7,7 @@ class Auditor():
         os.makedirs('log',exist_ok=True)
         logger = logging.getLogger('werkzeug')
         self.logger =logger 
-        logging.basicConfig(format='%(asctime)s %(message)s ',filename='log/login.log', encoding='utf-8',level=logging.INFO)
+        logging.basicConfig(format='%(asctime)s %(message)s ',filename='log/login.log', encoding='utf-8',level=logging.ERROR)
         self.logger.setLevel(logging.ERROR)
         
     
@@ -17,12 +17,12 @@ class Auditor():
     def registra(self,tipo,msg,usua="-"):
         client_ip = request.remote_addr
         if tipo==10:
-            self.logger.debug(client_ip+' '+msg+' '+usua)
+            self.logger.debug(client_ip+' '+msg+' ['+usua+']')
         elif tipo==20:
             self.logger.info(client_ip+' '+msg+' '+usua)
         elif tipo==30:
             self.logger.warning(client_ip+' '+msg+' '+usua)
         elif tipo==40:
-            self.logger.error(client_ip+' '+msg+' '+usua)
+            self.logger.error(client_ip+' '+msg+' ['+usua+']')
         elif tipo==50:
             self.logger.critical(client_ip+' '+msg+' '+usua)
